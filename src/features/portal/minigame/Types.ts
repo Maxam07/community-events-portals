08cc20833b25241214d3bde263bd226daa5cf9c6
Max <maxuberas@gmail.com>
2026-06-25 13:53:22 +0800

    [FEAT] Show equipped passive abilities in the mission panel

--- src/features/portal/minigame/Types.ts
+++ src/features/portal/minigame/Types.ts
@@ -170,6 +170,7 @@ export type DropItemType =
 export type BossTypes = "boss1" | "boss2" | "boss3";
 export type MobTypes = "mob1" | "mob2" | "mob3" | "mob4" | "mob5";
 export type CodexCategoryName = "Skills" | "Enemies" | "DropItems";
+export type PassiveAbilityType = "wings";
 
 export type EnemyConfig = {
   key: string;
---
+++ src/features/portal/minigame/components/panels/AbilityTooltip.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Box } from "components/ui/Box";
+import { InnerPanel } from "components/ui/Panel";
+import { IMMUNITY_TOOLTIP } from "../../constants";
+import { PassiveAbilityType } from "../../Types";
+
+interface ImmunityTooltipProps {
+  id: PassiveAbilityType;
+  image: string;
+  description?: string;
+  openId: string | null;
+  setOpenId: (id: string | null) => void;
+}
+
+export const AbilityTooltip: React.FC<ImmunityTooltipProps> = ({
+  id,
+  image,
+  openId,
+  setOpenId,
+}) => {
+  const isOpen = openId === id;
+
+  const handleClick = () => {
+    setOpenId(isOpen ? null : id);
+  };
+
+  const tooltipItem = IMMUNITY_TOOLTIP.find((item) => item.id === id);
+
+  return (
+    <div className="flex items-center relative">
+      <Box
+        image={image}
+        className="h-10 cursor-pointer"
+        onClick={handleClick}
+      />
+      {isOpen && tooltipItem && (
+        <InnerPanel className="absolute mt-6 w-[210px] left-1/2 transform -translate-x-1/2 -translate-y-full z-[999] pointer-events-auto">
+          <div className="m-1">
+            <div className="flex flex-row gap-2 items-center">
+              <img
+                src={tooltipItem.image}
+                className="h-5 mr-1"
+                alt="Crafting time"
+              />
+              <span className="text-xs">{tooltipItem.description}</span>
+            </div>
+          </div>
+        </InnerPanel>
+      )}
+    </div>
+  );
+};
